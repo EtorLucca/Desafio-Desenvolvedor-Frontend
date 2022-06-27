328870754dd40b9773e9ec2ff9774e48ab787d30
Étor Lucca <etor.lucca78@gmail.com>
2022-06-27 09:32:14 -0300

    Modificações para produção

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://apisimuladordeinvestimentos.glitch.me",
 });
 
 export const getIndicadores = async () => {
